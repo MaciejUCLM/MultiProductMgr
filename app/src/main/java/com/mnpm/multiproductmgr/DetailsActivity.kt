cd8364970246d43b98ca6ae1faf1cd39c641d338
Maciej Nalepa <maciej.nalepa@alu.uclm.es>
2021-05-17 12:25:40 +0200

    Renamed Activity files

--- app/src/main/java/com/mnpm/multiproductmgr/DetailsActivity.kt
+++ app/src/main/java/com/mnpm/multiproductmgr/DetailsActivity.kt
@@ -3,7 +3,7 @@ package com.mnpm.multiproductmgr
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Details : AppCompatActivity() {
+class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
--- app/src/main/java/com/mnpm/multiproductmgr/EditorActivity.kt
+++ app/src/main/java/com/mnpm/multiproductmgr/EditorActivity.kt
@@ -3,7 +3,7 @@ package com.mnpm.multiproductmgr
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Editor : AppCompatActivity() {
+class EditorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editor)
