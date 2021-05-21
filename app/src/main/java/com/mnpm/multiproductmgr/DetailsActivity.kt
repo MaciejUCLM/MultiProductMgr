3771bb8ce52968c730859e1c15eb109377283069
Maciej Nalepa <maciej.nalepa@alu.uclm.es>
2021-05-21 21:45:16 +0200

    Details activity actions

--- app/src/main/java/com/mnpm/multiproductmgr/DetailsActivity.kt
+++ app/src/main/java/com/mnpm/multiproductmgr/DetailsActivity.kt
@@ -1,12 +1,16 @@
 package com.mnpm.multiproductmgr
 
+import android.app.Activity
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 
-class DetailsActivity : AppCompatActivity() {
+class DetailsActivity : AppCompatActivity(), DeleteDialogListenerI {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
@@ -14,6 +18,11 @@ class DetailsActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         // Get a support ActionBar corresponding to this toolbar and enable the Up button
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // load provided product
+        val product = ProductManager.intentToProduct(intent)
+        product?.let {
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -28,12 +37,16 @@ class DetailsActivity : AppCompatActivity() {
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_edit -> {
-                // TODO action
+                val i = ProductManager.productToIntent(
+                        Intent(this, EditorActivity::class.java), ProductManager.getProduct())
+                startActivityForResult(i, 1235)
                 finish()
                 true
             }
             R.id.action_delete -> {
-                // TODO action
+                val dialog = DeleteDialog()
+                dialog.setParent(this)
+                dialog.show(supportFragmentManager, "delete")
                 finish()
                 true
             }
@@ -43,5 +56,27 @@ class DetailsActivity : AppCompatActivity() {
                 super.onOptionsItemSelected(item)
             }
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1235) {
+            if (resultCode == Activity.RESULT_OK) {
+                val product = ProductManager.intentToProduct(data)
+                if (product != null)
+                    ProductManager.products[ProductManager.getProductSelected()] = product
+            }
+        }
+    }
+
+    override fun deleteDialogAccepted(dialog: DialogInterface) {
+        ProductManager.removeSelectedProduct()
+        val toast = Toast.makeText(applicationContext, R.string.deleted, Toast.LENGTH_SHORT)
+        toast.show()
+    }
+
+    override fun deleteDialogCancelled(dialog: DialogInterface) {
+        val toast = Toast.makeText(applicationContext, R.string.cancel, Toast.LENGTH_SHORT)
+        toast.show()
     }
 }
